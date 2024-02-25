0769cbf91aaf102347be9b6d541d080837ca11e0
Jairo <hardtelles@gmail.com>
2024-02-25 20:15:20 -0300

    configuracao fase 5

--- env/prod/Backend.tf
+++
@@ -1,9 +0,0 @@
-
-# Configuração do backend para armazenar o estado do terraform no S3
-terraform {
-  backend "s3" {
-    bucket = "bucket-fiap-soat1-grupo56-remote-state"
-    key    = "fasteats-aplicacao-producao/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
--- infra/variaveis.tf
+++ infra/variaveis.tf
@@ -1,75 +1,97 @@
-variable "nome_repositorio" {
+variable "regiao" {
   type    = string
-  default = "repositorio-terraform-api-producao"
-}
-variable "cargoIam" {
-  type    = string
-  default = "fasteats-api-ms-producao"
+  default = "us-east-1"
 }
 
-variable "perfil" {
+variable "nome_repositorio" {
+  description = "Nome do repositório de imagens Docker"
   type    = string
-  default = "fasteats-api-ms-producao"
+  default = "microservico-cozinha"
 }
 
 variable "portaAplicacao" {
   type    = number
-  default = 8080
-}
-
-variable "ambiente" {
-  type    = string
-  default = "fasteats-api-cozinha"
-}
-
-variable "containerPort" {
-  type    = number
-  default = 8080
-}
-
-variable "containerName" {
-  type    = string
-  default = "fasteats-api-cozinha"
+  default = 8082
 }
 
 variable "imagemDocker" {
   type    = string
-  default = "fiap56soat1/fasteats-ms-producao-api:latest"
+  default = "730335661438.dkr.ecr.us-east-1.amazonaws.com/microserviceo-cozinha:latest"
 }
 
+variable "containerDbPort" {
+  type    = string
+  default = "27017"
+}
 
+variable "containerDbServer" {
+  type    = string
+  default = "localhost"
+}
 
-/*MONGO DB */
+variable "containerDbName" {
+  type    = string
+  default = "fasteatsproducaodb"
+}
 
+variable "containerDbUser" {
+  type    = string
+  default = "fiap56"
+}
 
-variable "APP_PORT" {
+variable "containerDbPassword" {
+  type    = string
+  default = "fiapsoat1grupo56"
+}
+
+variable "url_pagamento_service" {
+  type    = string
+  default = "http://ecs-fasteats-api-pagamento-399390289.us-west-2.elb.amazonaws.com"
+}
+
+variable "url_pedido_service" {
+  type    = string
+  default = "http://54.163.63.60"
+}
+
+variable "micro_servico" {
+  type    = string
+  default = "cozinha"
+}
+
+######### OBS: a execution role acima foi trocada por LabRole devido a restricoes de permissao na conta da AWS Academy ########
+variable "execution_role_ecs" {
+  type    = string
+  default = "arn:aws:iam::730335661438:role/LabRole" #aws_iam_role.ecsTaskExecutionRole.arn
+}
+
+variable "repositorio_url" {
+  default = "730335661438.dkr.ecr.us-east-1.amazonaws.com/microservico-cozinha"
+}
+
+variable "cpu_task" {
   type    = number
+  default = 256
 }
 
-variable "DB_USERNAME" {
-  type    = string
-}
-
-variable "DB_PASSWORD" {
-  type    = string
-}
-
-variable "DB_NAME" {
-  type    = string
-}
-
-variable "DB_PORT" {
+variable "memory_task" {
   type    = number
+  default = 512
 }
 
-variable "DB_HOST" {
-  type    = string
+variable "cpu_container" {
+  type    = number
+  default = 256
 }
 
-variable "URL_PEDIDO_SERVICE" {
-  type    = string
+variable "memory_container" {
+  type    = number
+  default = 512
 }
 
-variable "URL_PAGAMENTO_SERVICE" {
-  type    = string
+
+variable "container_insights" {
+  type        = bool
+  default     = false
+  description = "Set to true to enable container insights on the cluster"
 }
